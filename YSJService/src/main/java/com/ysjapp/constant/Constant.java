13ed239e78ae78309489728836bc6f684fd1dff2
Easter Dong <easyfisher@qq.com>
2015-05-03 23:42:57 +0800

    Add son response for debug.

---
+++ YSJService/src/main/java/com/ysjapp/constant/Constant.java
@@ -0,0 +1,6 @@
+package com.ysjapp.constant;
+
+public class Constant {
+	public static final String URL_HOME = "/home";
+	public static final String URL_SUB_USERS = "/users";
+}
--- YSJService/src/main/java/com/ysjapp/controller/HomeController.java
+++ YSJService/src/main/java/com/ysjapp/controller/HomeController.java
@@ -1,33 +1,37 @@
 package com.ysjapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ysjapp.model.UserModel;
 import com.ysjapp.model.proto.ProtoModels.UserList;
+import com.ysjapp.constant.Constant;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping(Constant.URL_HOME)
 public class HomeController {
-
-	public static void main(String[] args) {
-		UserList  userList = new HomeController().getUsers(3);
-		System.out.println(userList);
-	}
 	
-	@RequestMapping("/user")
+	@RequestMapping(Constant.URL_SUB_USERS)
 	public UserList getUsers(@RequestParam(value = "count", defaultValue = "1") int count) {
-//		List<UserModel> users = new ArrayList<UserModel>();
-//		for (int i = 0; i < count; i++) {
-//			users.add(UserModel.getRandomUser());
-//		}
 		
-//		return users;
 		UserList.Builder builder = UserList.newBuilder();
 		for (int i = 0; i < count; i++) {
 			builder.addUsers(UserModel.getRandomUser().generateMessage());
 		}
 		return builder.build();
+	}
+	
+	@RequestMapping("/users.json")
+	public List<UserModel> getUsersJson(@RequestParam(value = "count", defaultValue = "1") int count) {
+		List<UserModel> users = new ArrayList<UserModel>();
+		for (int i = 0; i < count; i++) {
+			users.add(UserModel.getRandomUser());
+		}
+		
+		return users;
 	}
 }
